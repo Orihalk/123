9f26bd5491162e070cdc75a55c599035a34f67b8
Orihalk <163280771+Orihalk@users.noreply.github.com>
2024-04-07 11:42:12 +0300

    Update cod.py

--- cod.py
+++ cod.py
@@ -1,4 +1,10 @@
-"""Module providing a function printing python version."""
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name
+# pylint: disable=trailing-newlines
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-final-newline
 
 import os
 import git
